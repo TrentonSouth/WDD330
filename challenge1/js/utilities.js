9ab85f63601d2ed718050bf8c2ab4d611eb49a3f
TrentonSouth <trenton@southfamily.com>
2021-02-09 07:13:43 -0600

    Fixed active error

--- challenge1/js/utilities.js
+++ challenge1/js/utilities.js
@@ -1,7 +1,5 @@
 export default class Utilities {
    clearActive() {
-      $('#all').removeClass('active');
-      $('#active').removeClass('active');
-      $('#completed').removeClass('active');
+      $('.menuButton').removeClass('active');
    }
 }
